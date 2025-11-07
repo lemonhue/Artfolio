10e25f5869750530b68f4b51a709892b7ae82358
lemonhue <lemonhue54@gmail.com>
2025-11-07 20:02:46 +0800

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="nav">
+        <a href="">Projects</a>
+        <a href="">About</a>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
