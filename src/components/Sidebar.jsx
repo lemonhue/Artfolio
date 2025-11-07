5bac9fda7049640570f9b01636ce24b2ab0c7c4f
lemonhue <lemonhue54@gmail.com>
2025-11-07 20:03:01 +0800

    add Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import "./Sidebar.scss"
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <h1>KKGURREN</h1>
+      <h1>KKGURREN</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
