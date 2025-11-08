8c48c10dd0126efc96bb1e29593f93fc00887a0b
lemonhue <lemonhue54@gmail.com>
2025-11-08 14:01:15 +0800

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,68 @@
-import "./Sidebar.scss"
+import "./Sidebar.scss";
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-      <h1>KKGURREN</h1>
-      <h1>KKGURREN</h1>
+        <div className="Banner">
+          <h1>KKGURREN</h1>
+          <h1>KKGURREN</h1>
+        </div>
+
+        <div className="stock-ticker-vertical">
+          <ul aria-hidden="true">
+            <li>
+              <span className="scroll-text"> I Draw</span>F
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+          </ul>
+
+          <ul aria-hidden="true">
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+            <li>
+              <span className="scroll-text"> I Draw</span>
+            </li>
+          </ul>
+      </div>
     </div>
   );
 }
