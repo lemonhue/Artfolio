91cc9c4e86e1889c4ea02acad8c9e8ff927c1a7c
lemonhue <lemonhue54@gmail.com>
2025-11-08 14:01:31 +0800

    add projects pages

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,18 @@
+import "./Projects.scss";
+import Navbar from "../components/navbar.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+
+
+function Projects() {
+  return (
+    <div className="Container">
+      <Navbar />
+      <Sidebar />
+      <div className="Projects-List">
+        
+        </div> 
+    </div>
+  );
+}
+
+export default Projects;
