dc9e2b704b6e075a473f087513aea094c5862481
lemonhue <lemonhue54@gmail.com>
2025-11-11 18:14:40 +0800

    add Google Auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
