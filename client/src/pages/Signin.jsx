ef2196853ed1e842b24bd05fe395ac493ce48a97
lemonhue <lemonhue54@gmail.com>
2025-11-12 20:39:41 +0800

    add sign in page

---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import "./Signin.scss";
+import Navbar from "../components/navbar.jsx";
+
+function Sign() {
+  return (
+    <div className="Container">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Sign;
