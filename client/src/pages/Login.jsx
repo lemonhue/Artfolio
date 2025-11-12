c175443e1319f5fb145f71db23c03f753a441452
lemonhue <lemonhue54@gmail.com>
2025-11-12 20:39:49 +0800

    add log in page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import "./Login.scss";
+import Navbar from "../components/navbar.jsx";
+
+function Login() {
+  return (
+
+    <div className="Container">
+        <Navbar />
+    </div>
+  );
+}
+
+export default Login;
