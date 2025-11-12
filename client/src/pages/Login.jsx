4ac1cba5a351317168683d8b90e2f2a9db26e02b
lemonhue <lemonhue54@gmail.com>
2025-11-12 21:40:21 +0800

    add google login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,17 @@
 import "./Login.scss";
 import Navbar from "../components/navbar.jsx";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 function Login() {
   return (
-
     <div className="Container">
-        <Navbar />
+      {/* <Navbar /> */}
+
+      <GoogleLogin
+        onSuccess={(credentialsResponse) => console.log(credentialsResponse)}
+        onError={() => console.log("Login failed")}
+      />
     </div>
   );
 }
