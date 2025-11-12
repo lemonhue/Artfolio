d5958391c5b20b21962f8590efc2d119a7dcd9e7
lemonhue <lemonhue54@gmail.com>
2025-11-12 20:28:43 +0800

    add private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,11 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Projects from "./pages/Projects.jsx"
+import Projects from "./pages/Projects.jsx";
+import PrivateRoutes from "./utilities/PrivateRoute.jsx";
+import AdminHome from "./pagegs/admin/Home.jsx";
+import AdminProjects from "./pagegs/admin/Projects.jsx";
+import AdminAbout from "./pagegs/admin/About.jsx";
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route index element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
-
+        <Route element={<PrivateRoutes />}>
+          <Route path="/AdminHome" element={<AdminHome />} />
+          <Route path="/AdminProjects" element={<AdminProjects />} />
+          <Route path="/AdminAbout" element={<AdminAbout />} />
+        </Route>
       </Routes>
     </>
   );
