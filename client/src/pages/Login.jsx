246bad6f697370148f4ca3710c1f96fb4da8d36b
lemonhue <lemonhue54@gmail.com>
2025-11-14 22:02:53 +0800

    add basic login features

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,14 +4,38 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
 function Login() {
+  const [email, setEmail] = UseState(null);
+  const [password, setPassword] = UseState(null);
+
+  handleSubmit = (event) => {};
+
   return (
     <div className="Container">
-      {/* <Navbar /> */}
+      <Navbar />
 
-      <GoogleLogin
-        onSuccess={(credentialsResponse) => console.log(credentialsResponse)}
-        onError={() => console.log("Login failed")}
-      />
+      <div className="Login-container">
+        <form>
+          <label>
+            Email:
+            <input type="text" name="email" />
+          </label>
+          <label>
+            Password:
+            <input type="text" name="password"></input>
+          </label>
+          <button type="submit" onClick={handleSubmit} />
+        </form>
+        <div className="Google-container">
+          <GoogleLogin
+            onSuccess={(credentialsResponse) =>
+              console.log(credentialsResponse)
+            }
+            onError={() => console.log("Login failed")}
+          />
+        </div>
+
+        <div className="Signin-container"></div>
+      </div>
     </div>
   );
 }
