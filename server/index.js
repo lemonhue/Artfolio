234bc7692109604f6d777c4342a1b0979e259abb
lemonhue <lemonhue54@gmail.com>
2025-11-14 21:48:25 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express() 
-app.use(express.json()) 
-app.use(cors())
+const AboutModel = require("./models/About");
+const AdminModel = require("./models/Admin");
+const CardModel = require("./models/Card");
+const CategoryModel = require("./models/Category");
+const SubcategoryModel = require("./models/Subcategory");
 
-mongoose.connect("mongodb://localhost:27017/card")
+const app = express();
+const PORT = 5000;
 
+app.use(express.json());
+app.use(cors());
 
+const router = express.Router();
+
+mongoose.connect("mongodb://localhost:27017/card");
 
 app.listen(3001, () => {
-    console.log("server is running")
-})
+  console.log("server is running");
+});
 
-
-
+app.get("/", (req, res) => {});
