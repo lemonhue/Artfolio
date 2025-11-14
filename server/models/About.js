a369b5d676f56ba0ab257f9e2e7aeee3e025672d
lemonhue <lemonhue54@gmail.com>
2025-11-14 22:03:57 +0800

    remove unecessary model attributes

--- server/models/About.js
+++ server/models/About.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const AboutSchema = new mongoose.Schema({
-  user_name: {
+  username: {
     type: String,
     required: true,
   },
--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  user_Id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true }
 });
 
 const UserModel = mongoose.model("User", UserSchema);
