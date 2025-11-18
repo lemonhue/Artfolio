cd6ccc28ef28cc144e68e18c71dc3bd9715a3929
lemonhue <lemonhue54@gmail.com>
2025-11-18 20:04:05 +0800

    fix error

--- server/index.js
+++ server/index.js
@@ -3,26 +3,35 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const AboutModel = require("./models/About");
-const AdminModel = require("./models/Admin");
-const CardModel = require("./models/Card");
-const CategoryModel = require("./models/Category");
-const SubcategoryModel = require("./models/Subcategory");
-
+// const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT;
 const DB_Host = process.env.DB_HOST;
+const adminRoutes = require("./routes/Admin");
+const cardRoutes = require("./routes/Card");
+const categoryRoutes = require("./routes/Category");
+const subcategoryRoutes = require("./routes/Subcategory");
+const aboutRoutes = require("./routes/About");
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
 
 const router = express.Router();
 
-mongoose.connect(DB_Host);
+mongoose
+  .connect(DB_Host)
+  .then(async () => {
+    console.log("Successfully connected to DB");
+  })
+  .catch((error) => console.log(error));
 
-app.listen(5173, () => {
+app.listen(PORT, () => {
   console.log("server is running");
 });
 
-app.get("/", (req, res) => {});
+app.use("/admin", adminRoutes);
+app.use("/card", cardRoutes);
+app.use("/about", cardRoutes);
+app.use("/category", cardRoutes);
+app.use("/subcategory", cardRoutes);
