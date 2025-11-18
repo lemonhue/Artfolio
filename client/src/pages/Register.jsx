e0c91a82214df4d10669e76a92ee410bcdcd27bb
lemonhue <lemonhue54@gmail.com>
2025-11-18 21:30:02 +0800

    update ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,76 +1,109 @@
 import "./Register.scss";
 import Navbar from "../components/navbar.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
 function Register() {
-  const [firstName, setFirstName] = useState(null);
-  const [lastName, setLastName] = useState(null);
-  const [userName, setUserName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [showButton, setShowButton] = useState(false);
 
   const handleSubmit = async () => {
     try {
       const response = await axios.post(
-        "/",
-        JSON.stringify({ firstName, lastName, userName, email, password }),
+        "http://localhost:5000/admin",
+        { firstName, lastName, userName, email, password },
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       );
+      if (response.status === 200 || response.status === 201) {
+        console.log("Account successfully created!");
+        navigate("/login");
+      }
     } catch (error) {
       console.error("Error during registration:", error);
     }
   };
 
+  useEffect(() => {
+    if (firstName && lastName && userName && email && password) {
+      setShowButton(true);
+    }
+  }, [firstName, lastName, userName, email, password]);
+
   return (
     <div className="Container">
-      <Navbar />
-
       <div className="Register-Container">
         <form onSubmit={handleSubmit}>
-          <label> First Name:</label>
-          <Input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-          <label> Last Name:</label>
-          <Input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-          <label> UserName:</label>
-          <Input
-            type="text"
-            id="userName"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            required
-          />
-          <label> Email:</label>
-          <Input
-            type="text"
-            id="email"
-            value={lastName}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label> Password:</label>
-          <Input
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <div className="input-group">
+            <label> First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label> Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label> Username:</label>
+            <input
+              type="text"
+              id="userName"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label> Email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label> Password:</label>
+            <input
+              type="text"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
         </form>
+        <div className="container-button">
+          {showButton && (
+            <button
+              type="button"
+              className="submit-button"
+              onClick={handleSubmit}
+            >
+              submit
+              </button>
+          )}
+        </div>
       </div>
     </div>
   );
