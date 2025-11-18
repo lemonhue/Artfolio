997038dc603b6e436103daeffbbbd1007925e8c7
lemonhue <lemonhue54@gmail.com>
2025-11-18 22:11:07 +0800

    update ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,54 +3,64 @@ import { useState } from "react";
 import Navbar from "../components/navbar.jsx";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 function Login() {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  handleSubmit = async (event) => {
-    e.preventDefault();
+  const handleSubmit = async (event) => {
     try {
-      const response = await axios.post(
-        "/",
-        JSON.stringify({ email, password }),
+      const response = await axios.get(
+        "/admin",
+        { email, password },
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       );
+      if (response.status === 200 || response.status === 201) {
+        console.log("Succesfully logged in!");
+      }
     } catch (error) {
       console.error("Error during login:", error);
     }
   };
 
   return (
-    <div className="Container">
-      <Navbar />
-
+    <div className="Main-Container">
+      <div className="Containerr">
       <div className="Login-container">
         <form>
-          <label>
-            Email:
+          <div className="input-group">
+            <label>Email: </label>
+
             <input type="text" name="email" />
-          </label>
-          <label>
-            Password:
+          </div>
+
+          <div className="input-group">
+            <label> Password: </label>
+
             <input type="text" name="password"></input>
-          </label>
+          </div>
           <button type="submit" onClick={handleSubmit} />
         </form>
-        <div className="Google-container">
-          <GoogleLogin
-            onSuccess={(credentialsResponse) =>
-              console.log(credentialsResponse)
-            }
-            onError={() => console.log("Login failed")}
-          />
+        <div className="login-button-container">
+          <button className="login-button">Login</button>
         </div>
-
-        <div className="Signin-container"></div>
       </div>
+      <div className="Google-container">
+        <GoogleLogin
+          onSuccess={(credentialsResponse) => console.log(credentialsResponse)}
+          onError={() => console.log("Login failed")}
+        />
+      </div>
+      <div className="Signin-container">
+        <button className="register-button">Create Account</button>
+      </div>
+    </div>
     </div>
   );
 }
