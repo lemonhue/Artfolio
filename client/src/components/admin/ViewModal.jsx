812fa1b2d6a21b2ced5c34981f43c8f6e418c2ee
lemonhue <lemonhue54@gmail.com>
2025-11-29 20:55:16 +0800

    create view modal

---
+++ client/src/components/admin/ViewModal.jsx
@@ -0,0 +1,38 @@
+import "./ViewModal.scss";
+import ReactDom from "react-dom";
+
+function ViewModal({ card, open, onClose }) {
+  if (!open) return null;
+
+  const handleDelete = async (e) => {
+    try {
+      const response = await axios.delete("/", {
+        withCredentials: true,
+      });
+      if (response.status === 200 || response.status === 201) {
+        console.log("Card deleted succesfully!");
+      }
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+
+  return ReactDom.createPortal(
+    <>
+      <div className="overlay" />
+      <div className="container-modal">
+        <div className="container view">
+          <div className="container-delete-button">
+            <button className="delete-button" onClick={handleDelete}>
+              Delete
+            </button>
+          </div>
+          <img src={card.image} />
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+}
+
+export default ViewModal;
