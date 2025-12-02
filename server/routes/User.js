0147c2a26d0093cc89a0e7e17a6a011a9f02c92e
lemonhue <lemonhue54@gmail.com>
2025-12-02 12:50:18 +0800

    fix login

--- server/routes/User.js
+++ server/routes/User.js
@@ -9,22 +9,24 @@ router.get("/", async (req, res) => {
   return res.status(200).json(users);
 });
 
-router.get("/:id", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { email, password } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ message: "No such user exists!" });
+    const user = await User.findOne({ email });
+    {
+      if (!user)
+        return res.status(404).json({ message: "User does not exist." });
     }
 
-    const user = await User.findById(id);
-
-    if (!user) {
-      return res.status(404).json({ message: "No such user exists!" });
+    if (user.password !== password) {
+      return res.status(404).json({ message: "Invalid password" });
     }
-
-    return res.status(200).json(card);
-  } catch {}
+    return res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    console.error("error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -50,8 +52,5 @@ router.post("/", async (req, res) => {
     return res.status(400).json({ message: error.mmessage });
   }
 });
-
-
-
 
 module.exports = router;
