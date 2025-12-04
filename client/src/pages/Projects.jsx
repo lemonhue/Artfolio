5d072ff823cb38b3762fcaee8c2f90c07ccbdf82
lemonhue <lemonhue54@gmail.com>
2025-12-04 21:02:23 +0800

    flex-start ul

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,17 +1,19 @@
 import "./Projects.scss";
-import Navbar from "../components/admin/AdminNavbar.jsx";
+import Navbar from "../components/Navbar.jsx";
 import Sidebar from "../components/Sidebar.jsx";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import Pagination from "../components/Pagination.jsx";
+import ViewModal from "../components/viewModal.jsx";
 
 function Projects() {
   const [cards, setCards] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
   const [hoveredCard, setHoveredCard] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(11);
-
+  const [postsPerPage, setPostsPerPage] = useState(12);
+  const [selectedCard, setSelectedCard] = useState(null);
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
   const currentPosts = cards.slice(firstPostIndex, lastPostIndex);
@@ -53,16 +55,17 @@ function Projects() {
                       src={card.image}
                       onClick={() => {
                         setSelectedCard(card);
-                        setIsViewOpen(true);
+                        setIsOpen(true);
                       }}
                     />
                   </li>
                 ))}
-              <div className="Cards-Placeholder">
-                <button onClick={() => setIsOpen(true)}>
-                  <IoIosAddCircleOutline />
-                </button>
-              </div>
+
+              <ViewModal
+                open={isOpen}
+                card={selectedCard}
+                onClose={() => setIsOpen(false)}
+              />
             </ul>
           </div>
           <div className="Public-Bottom-Section">
