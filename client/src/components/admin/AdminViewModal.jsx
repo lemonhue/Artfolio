0f3b5aa8748769817e2caa5557f8463c018d8293
lemonhue <lemonhue54@gmail.com>
2025-12-04 20:54:59 +0800

    update

--- client/src/components/admin/AdminViewModal.jsx
+++ client/src/components/admin/AdminViewModal.jsx
@@ -7,7 +7,7 @@ function AdminViewModal({ card, open, onClose }) {
   if (!open) return null;
 
   const handleDelete = async () => {
-        try {
+    try {
       const response = await axios.delete(
         `http://localhost:5000/card/${card._id}`,
         {
@@ -27,12 +27,33 @@ function AdminViewModal({ card, open, onClose }) {
     <>
       <div className="overlay" />
       <div className="container-public-view-modal">
-        <img src={card.image} />
+        <div className="container-view-modal-content">
+          <div className="container-section1">
+            <img src={card.image} />
+          </div>
 
-        <div className="container-buttons">
-          <div className="container-delete-button">
-            <button onClick={onClose}> close</button>
-            <button onClick={handleDelete}> delete</button>
+          <div className="container-section2">
+            <input
+              className="title"
+              name="title"
+              type="text"
+              value={card.title}
+              placeholder="Enter Project Title"
+            />
+            {/* </div> */}
+
+            {/* <div className="input-group-description"> */}
+            <textarea
+              className="description"
+              name="description"
+              type="text"
+              value={card.description}
+              placeholder="Enter Project Description"
+            />
+            <div className="container-buttons">
+              <button onClick={onClose}> close</button>
+              <button onClick={handleDelete}> delete</button>
+            </div>
           </div>
         </div>
       </div>
