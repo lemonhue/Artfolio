e73fe0251f629c67a0aa7bd73f129acbcf1f1785
lemonhue <lemonhue54@gmail.com>
2025-12-05 19:57:02 +0800

    refactor: update local strat

---
+++ server/middleware/passport.js
@@ -0,0 +1,66 @@
+var passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+var GoogleStrategy = require("passport-google-oauth20");
+var User = require("../models/User");
+const crypto = require("crypto");
+
+GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user)
+          return done(null, false, { message: "Incorrect email or password" });
+
+        const isValid = verifyPassword(password, user.salt, user.passwordHash);
+        if (!isValid)
+          return done(null, false, { message: "Incorrect email or password" });
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+function verifyPassword(password, salt, hash) {
+  const hashedPassword = crypto
+    .scryptSync(password, salt, 64, { N: 16384, r: 8, p: 1 })
+    .toString("hex");
+  return hashedPassword === hash;
+}
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findUserById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// module.exports = { verifyPassword, passport };
