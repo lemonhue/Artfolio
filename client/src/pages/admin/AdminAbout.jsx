d00183dcf4479cd17e2563c699669d5db97e06a5
lemonhue <lemonhue54@gmail.com>
2025-12-10 18:55:37 +0800

    feature: add update function

--- client/src/pages/admin/AdminAbout.jsx
+++ client/src/pages/admin/AdminAbout.jsx
@@ -7,22 +7,45 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import Modal from "../../components/admin/CardModal.jsx";
 import Pagination from "../../components/Pagination.jsx";
 import ViewModal from "../../components/admin/AdminViewModal.jsx";
+import { MdModeEdit } from "react-icons/md";
+import { FaSave } from "react-icons/fa";
 
 function AdminAbout() {
   const [isOpen, setIsOpen] = useState(false);
   const [isViewOpen, setIsViewOpen] = useState(false);
+  const [isEditNameClicked, setIsEditNameClicked] = useState(false);
+  const [isEditBioClicked, setIsEditBioClicked] = useState(false);
   const [about, setAbout] = useState(null);
 
   const fetchAbout = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/about`);
+      const response = await axios.get(`http://localhost:5000/about/admin`, {
+        withCredentials: true,
+      });
 
       if (response.status === 200) {
-        setAbout(response.data);
+        setAbout(response.data[0]);
         console.log("about:", response.data);
       }
     } catch (error) {
       console.error("error:", error);
+    }
+  };
+  const updateAbout = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/about/${about._id}`,
+        about,
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error("error:", error.response?.data || error.message);
     }
   };
 
@@ -37,8 +60,60 @@ function AdminAbout() {
         {/* <Sidebar /> */}
         <div className="Admin-About-Container">
           <div className="Admin-About-Content">
-            {/* <h1>KKGURREN</h1> */}
-            <p>I am an artist based in PH</p>
+            <div className="Admin-About-Username">
+              {isEditNameClicked ? (
+                <>
+                  <input
+                    value={about?.username}
+                    onChange={(e) =>
+                      setAbout({ ...about, username: e.target.value })
+                    }
+                  />
+                  <FaSave
+                    className="icon"
+                    onClick={() => {
+                      updateAbout();
+                      setIsEditNameClicked(false);
+                    }}
+                  />
+                </>
+              ) : (
+                <>
+                  <span>{about?.username || ""}</span>
+                  <MdModeEdit
+                    className="icon"
+                    onClick={() => setIsEditNameClicked(true)}
+                  />
+                </>
+              )}
+            </div>
+            <div className="Admin-About-Bio">
+              {isEditBioClicked ? (
+                <>
+                  <textarea
+                    value={about?.bio}
+                    onChange={(e) =>
+                      setAbout({ ...about, bio: e.target.value })
+                    }
+                  />
+                  <FaSave
+                    className="icon"
+                    onClick={() => {
+                      updateAbout();
+                      setIsEditBioClicked(false);
+                    }}
+                  />
+                </>
+              ) : (
+                <>
+                  <span>{about?.bio || ""}</span>
+                  <MdModeEdit
+                    className="icon"
+                    onClick={() => setIsEditBioClicked(true)}
+                  />
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
