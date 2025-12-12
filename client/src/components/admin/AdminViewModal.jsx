785ee769c3ea6a29f37c5c2d5462a5e5ff44a58c
lemonhue <lemonhue54@gmail.com>
2025-12-12 18:14:49 +0800

    feat: add update project function

--- client/src/components/admin/AdminViewModal.jsx
+++ client/src/components/admin/AdminViewModal.jsx
@@ -1,9 +1,11 @@
 import "./AdminViewModal.scss";
 import ReactDom from "react-dom";
+import { useState, useEffect } from "react";
+
 import axios from "axios";
 import { FaTrash } from "react-icons/fa";
 
-function AdminViewModal({ card, open, onClose }) {
+function AdminViewModal({ card, setCard, open, onClose }) {
   if (!open) return null;
 
   const handleDelete = async () => {
@@ -23,6 +25,24 @@ function AdminViewModal({ card, open, onClose }) {
     }
   };
 
+  const updateProject = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/card/${card._id}`,
+        card,
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error("error:", error.response?.data || error.message);
+    }
+  };
+
   return ReactDom.createPortal(
     <>
       <div className="overlay" />
@@ -38,21 +58,22 @@ function AdminViewModal({ card, open, onClose }) {
               name="title"
               type="text"
               value={card.title}
-              placeholder="Enter Project Title"
+              onChange={(e) => setCard({ ...card, title: e.target.value })}
             />
-            {/* </div> */}
 
-            {/* <div className="input-group-description"> */}
             <textarea
               className="description"
               name="description"
               type="text"
               value={card.description}
-              placeholder="Enter Project Description"
+              onChange={(e) =>
+                setCard({ ...card, description: e.target.value })
+              }
             />
             <div className="container-buttons">
               <button onClick={onClose}> close</button>
               <button onClick={handleDelete}> delete</button>
+              <button onClick={updateProject}> update</button>
             </div>
           </div>
         </div>
