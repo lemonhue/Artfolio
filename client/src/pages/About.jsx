3b5bab056438d60a950e05507fa505db8f01f0da
lemonhue <lemonhue54@gmail.com>
2025-12-20 21:08:30 +0800

    feat: add function for fetching about data

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,44 @@
 import "./About.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Navbar from "../components/Navbar.jsx";
 
 function About() {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isViewOpen, setIsViewOpen] = useState(false);
+  const [about, setAbout] = useState(null);
+
+  const fetchAbout = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/about");
+      if (response.status === 200) {
+        console.log("About fetched successfully");
+        setAbout(response.data[0]);
+        console.log(response.data);
+      }
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAbout();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="Container-Public-About">
+        <div className="Public-About-Container">
+          <div className="Public-About-Content">
+            <div className="Public-About-Username"> {about?.username}</div>
+            <div className="Public-About-Bio">{about?.bio}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
