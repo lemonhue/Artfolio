5002eef3015046159c6ec851d6e1e2a45ed1c0b7
lemonhue <lemonhue54@gmail.com>
2026-01-02 12:11:33 +0800

    feat: add function to check auth-status

--- client/src/utilities/PrivateRoute.jsx
+++ client/src/utilities/PrivateRoute.jsx
@@ -1,9 +1,34 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function PrivateRoutes() {
-  const auth = { token: true };
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  return auth.token ? <Outlet /> : <Navigate to="/login" />;
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await axios.get("http://localhost:5000/user/auth-status", {
+          withCredentials: true,
+        });
+        setIsAuthenticated(true);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
 }
 
 export default PrivateRoutes;
