b841dc064dacc3235b6254674d06f3e6d84d93f5
lemonhue <lemonhue54@gmail.com>
2026-01-02 12:24:22 +0800

    fix: typo

--- server/middleware/ensureAuth.js
+++ server/middleware/ensureAuth.js
@@ -5,7 +5,7 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next(); 
   }
-  return res.status(401).json({ message: "Unauthorizedss" });
+  return res.status(401).json({ message: "Unauthorized" });
 }
 
 
