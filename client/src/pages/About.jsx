7abdb6ce422a1202c82e4ea990df5c91d2d71a84
lemonhue <lemonhue54@gmail.com>
2026-01-07 16:01:34 +0800

    feat: use h2 for bio

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -32,8 +32,8 @@ function About() {
       <div className="Container-Public-About">
         <div className="Public-About-Container">
           <div className="Public-About-Content">
-              <h1 className="Public-About-Username">{about?.username}</h1>
-              <h1 className="Public-About-Bio">{about?.bio}</h1>
+            <h1 className="Public-About-Username">{about?.username}</h1>
+            <h2 className="Public-About-Bio">{about?.bio}</h2>
           </div>
         </div>
       </div>
